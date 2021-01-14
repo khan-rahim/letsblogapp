65b6d505de9c7a80620fbe988b391c25e6128dd2
Rahim <khanmrahim19@gmail.com>
2021-01-14 18:59:12 +0530

    final

--- django_project/settings.py
+++ django_project/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '4_j+_%1ms#2(2u&t8$4j#(t55-vsts2703dp+7$x=81p2zfop@'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
